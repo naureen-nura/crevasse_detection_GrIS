588785ed708c1d82462991759f7d7abda467301b
Naureen Khan <naureenk@buffalo.edu>
2023-07-09 05:54:51 +0000

    update image/label path and jpeg to png

--- src/unet/crevasse_data.py
+++ src/unet/crevasse_data.py
@@ -1,16 +1,15 @@
 import os
-import cv2
 import numpy as np
 from glob import glob
-import tensorfprintlow as tf
+import tensorflow as tf
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-IMAGE_SIZE = 500,500
+IMAGE_SIZE = 1000,1000
 channels = 1
 num_classes = 3
-batch = 2
+batch = 1
 #LR = 1e-4
 #EPOCHS = 5
 
@@ -22,19 +21,19 @@ test_data = "Test/"
 
 def load_data():
 
-  TRAIN_X = sorted(glob(os.path.join(image_path + training_data, "*.jpeg")))
+  TRAIN_X = sorted(glob(os.path.join(image_path + training_data, "*.png")))
   train_x = TRAIN_X[:]
-  TRAIN_Y = sorted(glob(os.path.join(mask_path + training_data, "*.jpeg")))
+  TRAIN_Y = sorted(glob(os.path.join(mask_path + training_data, "*.png")))
   train_y = TRAIN_Y[:]
 
-  VALID_X = sorted(glob(os.path.join(image_path + val_data, "*.jpeg")))
+  VALID_X = sorted(glob(os.path.join(image_path + val_data, "*.png")))
   valid_x = VALID_X[:]
-  VALID_Y = sorted(glob(os.path.join(mask_path + val_data, "*.jpeg")))
+  VALID_Y = sorted(glob(os.path.join(mask_path + val_data, "*.png")))
   valid_y = VALID_Y[:]
 
-  TEST_X = sorted(glob(os.path.join(image_path + test_data, "*.jpeg")))
+  TEST_X = sorted(glob(os.path.join(image_path + test_data, "*.png")))
   test_x = TEST_X[:]
-  TEST_Y = sorted(glob(os.path.join(mask_path + test_data, "*.jpeg")))
+  TEST_Y = sorted(glob(os.path.join(mask_path + test_data, "*.png")))
   test_y = TEST_Y[:]
 
   return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
@@ -50,17 +49,17 @@ data_augmentation = tf.keras.Sequential([
 
 def read_image(path):
     x = tf.io.read_file(path)
-    x = tf.image.decode_jpeg(x, channels=0)
-    x = tf.image.resize(x, [500, 500])
-    x = tf.cast(x, dtype=tf.uint8)
+    x = tf.image.decode_png(x, channels=0)
+    #x = tf.image.resize(x, [500, 500])
+    #x = tf.cast(x, dtype=tf.uint8)
     #x = tf.expand_dims(x[:,:,:,0], axis=-1)
     return x
 
 def read_mask(path):
     y = tf.io.read_file(path)
-    y = tf.image.decode_jpeg(y, channels=0)
-    y = tf.image.resize(y, [500,500])
-    y = tf.cast(y, dtype=tf.uint8)
+    y = tf.image.decode_png(y, channels=0)
+    #y = tf.image.resize(y, [500,500])
+    #y = tf.cast(y, dtype=tf.uint8)
     #y = tf.expand_dims(y[:,:,:,0], axis=-1)
     return y
  
@@ -80,7 +79,7 @@ num_threads = 4
 
 def tf_dataset_train(x, y, batch=2):
     dataset = tf.data.Dataset.from_tensor_slices((x, y))
-    dataset = dataset.shuffle(buffer_size=10) #Buffer size needs to be greater or equal to the full size of the dataset
+    dataset = dataset.shuffle(buffer_size=4) #Buffer size needs to be greater or equal to the full size of the dataset
     dataset = dataset.repeat()
     dataset = dataset.map(tf_parse, num_parallel_calls=num_threads)
     dataset = dataset.batch(batch)
@@ -91,7 +90,7 @@ def tf_dataset_train(x, y, batch=2):
 
 def tf_dataset_valid(x, y, batch=1):
     dataset = tf.data.Dataset.from_tensor_slices((x, y))
-    dataset = dataset.shuffle(buffer_size=4)
+    dataset = dataset.shuffle(buffer_size=1)
     dataset = dataset.repeat()
     dataset = dataset.map(tf_parse, num_parallel_calls=num_threads)
     dataset = dataset.batch(batch)
